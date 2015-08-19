2a0b5c847fa63a8e8a19a8e78d435b8bf1d83bf6
ilammy <a.lozovsky@gmail.com>
2015-08-19 10:25:59 +0300

    x11/cliprdr: transfer raw format data
    The second step of raw transfer is to transfer the format data itself.
    This has been already implemented in XFreeRDP before, but several
    tweaks are required for it to work correctly.
    
    The idea of raw data transfer is to request for _FREERDP_RAW clipboard
    format while putting the actual formatId into _FREERDP_CLIPRDR property
    where the requested data is expected to arrive to. Then the clipboard
    owner will check for the real formatId and deliver the expected data.
    
    This stays true, but the check is performed in a more straightforward
    way, and CF_RAW format (numerically equal to zero) is not considered
    an unknown destination format when performing (identity) conversions
    with wClipboard. This is not an issue because wClipboard will allow
    only identity conversion for CF_RAW, it will fail if something else
    is going to be converted into CF_RAW.

--- client/X11/xf_cliprdr.c
+++ client/X11/xf_cliprdr.c
@@ -77,6 +77,7 @@ struct xf_clipboard
 	int requestedFormatId;
 
 	BYTE* data;
+	BOOL data_raw_format;
 	UINT32 data_format_id;
 	const char* data_format_name;
 	int data_length;
@@ -498,6 +499,10 @@ static void xf_cliprdr_process_requested_data(xfClipboard* clipboard, BOOL hasDa
 
 	switch (format->formatId)
 	{
+		case CF_RAW:
+			srcFormatId = CF_RAW;
+			break;
+
 		case CF_TEXT:
 		case CF_OEMTEXT:
 		case CF_UNICODETEXT:
@@ -537,7 +542,7 @@ static void xf_cliprdr_process_requested_data(xfClipboard* clipboard, BOOL hasDa
 		dstFormatId = format->formatId;
 	}
 
-	if (bSuccess && dstFormatId)
+	if (bSuccess)
 	{
 		DstSize = 0;
 		pDstData = (BYTE*) ClipboardGetData(clipboard->system, dstFormatId, &DstSize);
@@ -718,6 +723,7 @@ static BOOL xf_cliprdr_process_selection_request(xfClipboard* clipboard, XEvent*
 	XEvent* respond;
 	BYTE* data = NULL;
 	BOOL delayRespond;
+	BOOL rawTransfer;
 	unsigned long length;
 	unsigned long bytes_left;
 	CLIPRDR_FORMAT* format;
@@ -756,6 +762,7 @@ static BOOL xf_cliprdr_process_selection_request(xfClipboard* clipboard, XEvent*
 		{
 			formatId = format->formatId;
 			formatName = format->formatName;
+			rawTransfer = FALSE;
 
 			if (formatId == CF_RAW)
 			{
@@ -768,6 +775,7 @@ static BOOL xf_cliprdr_process_selection_request(xfClipboard* clipboard, XEvent*
 
 				if (data)
 				{
+					rawTransfer = TRUE;
 					CopyMemory(&formatId, data, 4);
 					XFree(data);
 				}
@@ -799,6 +807,7 @@ static BOOL xf_cliprdr_process_selection_request(xfClipboard* clipboard, XEvent*
 				clipboard->respond = respond;
 				clipboard->data_format_id = formatId;
 				clipboard->data_format_name = formatName;
+				clipboard->data_raw_format = rawTransfer;
 				delayRespond = TRUE;
 
 				xf_cliprdr_send_data_request(clipboard, formatId);
@@ -1094,12 +1103,15 @@ static int xf_cliprdr_server_format_list_response(CliprdrClientContext* context,
 
 static int xf_cliprdr_server_format_data_request(CliprdrClientContext* context, CLIPRDR_FORMAT_DATA_REQUEST* formatDataRequest)
 {
+	BOOL rawTransfer;
 	xfCliprdrFormat* format = NULL;
 	UINT32 formatId = formatDataRequest->requestedFormatId;
 	xfClipboard* clipboard = (xfClipboard*) context->custom;
 	xfContext* xfc = clipboard->xfc;
 
-	if (xf_cliprdr_is_raw_transfer_available(clipboard))
+	rawTransfer = xf_cliprdr_is_raw_transfer_available(clipboard);
+
+	if (rawTransfer)
 	{
 		format = xf_cliprdr_get_client_format_by_id(clipboard, CF_RAW);
 
@@ -1117,7 +1129,7 @@ static int xf_cliprdr_server_format_data_request(CliprdrClientContext* context, 
 		return 1;
 	}
 
-	clipboard->requestedFormatId = formatId;
+	clipboard->requestedFormatId = rawTransfer ? CF_RAW : formatId;
 
 	XConvertSelection(xfc->display, clipboard->clipboard_atom,
 		format->atom, clipboard->property_atom, xfc->drawable, CurrentTime);
@@ -1159,7 +1171,12 @@ static int xf_cliprdr_server_format_data_response(CliprdrClientContext* context,
 	srcFormatId = 0;
 	dstFormatId = 0;
 
-	if (clipboard->data_format_name)
+	if (clipboard->data_raw_format)
+	{
+		srcFormatId = CF_RAW;
+		dstFormatId = CF_RAW;
+	}
+	else if (clipboard->data_format_name)
 	{
 		if (strcmp(clipboard->data_format_name, "HTML Format") == 0)
 		{
@@ -1210,7 +1227,7 @@ static int xf_cliprdr_server_format_data_response(CliprdrClientContext* context,
 	if (!bSuccess)
 		free (pSrcData);
 
-	if (bSuccess && dstFormatId)
+	if (bSuccess)
 	{
 		DstSize = 0;
 		pDstData = (BYTE*) ClipboardGetData(clipboard->system, dstFormatId, &DstSize);
