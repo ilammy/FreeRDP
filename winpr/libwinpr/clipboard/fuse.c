83b06a62b07abb17f53c69087dbbb832e6b027bd
ilammy <a.lozovsky@gmail.com>
2017-07-27 17:30:20 -0400

    wClipboard/fuse: create and cleanup mount point
    FUSE will need a directory to mount to so we should create one. We also
    should remove that directory when we're done and don't need it anymore.
    
    First try to create a directory in /var/run/user hierarchy available on
    many Linux systems, and if that fails then fall back to using /tmp.
    
    When creating a directory make sure that all parent directories are
    present as well. Also don't fail if the directory is already there.
    
    Make sure that different FreeRDP processes launched by different users
    get different directories which can be used independently.

--- winpr/libwinpr/clipboard/fuse.c
+++ winpr/libwinpr/clipboard/fuse.c
@@ -21,9 +21,17 @@
 #include "config.h"
 #endif
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include <winpr/clipboard.h>
+#include <winpr/string.h>
 #include <winpr/wlog.h>
 
 #include "clipboard.h"
@@ -34,7 +42,109 @@
 
 struct fuse_subsystem_context
 {
+	char* mount_point;
 };
+
+static BOOL ensure_directory(const char* path)
+{
+	int err = 0;
+	struct stat buf;
+
+	errno = 0;
+
+	if (mkdir(path, 0775) == 0)
+		return TRUE;
+
+	if (errno == EEXIST)
+	{
+		errno = 0;
+
+		if (stat(path, &buf) == 0)
+			return S_ISDIR(buf.st_mode);
+	}
+
+	err = errno;
+	WLog_DBG(TAG, "failed to make directory %s: %s", path, strerror(err));
+	return FALSE;
+}
+
+static void remove_directory(const char* path)
+{
+	int err = 0;
+
+	errno = 0;
+
+	if (rmdir(path) == 0)
+		return;
+
+	err = errno;
+	WLog_DBG(TAG, "failed to remove directory %s: %s", path, strerror(err));
+}
+
+static BOOL ensure_mount_point_at(struct fuse_subsystem_context* subsystem,
+		char* path)
+{
+	char* delimiter;
+
+	if (path[0] == '\0')
+		return FALSE;
+
+	/* mkdir -p */
+	for (delimiter = path + 1; *delimiter; delimiter++)
+	{
+		if (*delimiter == '/')
+		{
+			*delimiter = '\0';
+			if (!ensure_directory(path))
+				return FALSE;
+			*delimiter = '/';
+		}
+	}
+	if (!ensure_directory(path))
+		return FALSE;
+
+	subsystem->mount_point = _strdup(path);
+	if (!subsystem->mount_point)
+	{
+		remove_directory(path);
+		return FALSE;
+	}
+
+	WLog_DBG(TAG, "initialized FUSE mount point at %s", path);
+
+	return TRUE;
+}
+
+static BOOL ensure_mount_point(struct fuse_subsystem_context* subsystem)
+{
+	char path[256];
+	pid_t pid = getpid();
+	uid_t uid = getuid();
+
+	snprintf(path, sizeof(path), "/var/run/user/%u/winpr/clipboard-%d", uid, pid);
+	if (ensure_mount_point_at(subsystem, path))
+		return TRUE;
+
+	snprintf(path, sizeof(path), "/tmp/winpr/%u/clipboard-%d", uid, pid);
+	if (ensure_mount_point_at(subsystem, path))
+		return TRUE;
+
+	WLog_DBG(TAG, "failed to initialize FUSE mount point");
+	return FALSE;
+}
+
+static void remove_mount_point(struct fuse_subsystem_context* subsystem)
+{
+	/*
+	 * Despite the fact that we can create the whole directory chain for
+	 * a path like "/var/run/user/XXXX/winpr/clipboard-YYYY", we remove
+	 * only the terminating "clipboard-YYYY" directory because the parent
+	 * directories may be currently in use by other processes.
+	 */
+	remove_directory(subsystem->mount_point);
+	free(subsystem->mount_point);
+	subsystem->mount_point = NULL;
+}
 
 static struct fuse_subsystem_context* make_subsystem_context(void)
 {
@@ -44,7 +154,15 @@ static struct fuse_subsystem_context* make_subsystem_context(void)
 	if (!subsystem)
 		return NULL;
 
+	if (!ensure_mount_point(subsystem))
+		goto error_free_subsystem;
+
 	return subsystem;
+
+error_free_subsystem:
+	free(subsystem);
+
+	return NULL;
 }
 
 static void free_subsystem_context(void* context)
@@ -53,6 +171,8 @@ static void free_subsystem_context(void* context)
 
 	if (subsystem)
 	{
+		remove_mount_point(subsystem);
+
 		free(subsystem);
 	}
 }
